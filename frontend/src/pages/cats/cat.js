fc06913a8e0d8a80eb99d60981acb5bb53c378b2
Bernnardo <bernnardosaores@hotmail.com>
2023-01-11 17:48:14 -0300

    Pagina de Dog e Cat feitos

--- frontend/src/pages/cats/cat.js
+++ frontend/src/pages/cats/cat.js
@@ -1,79 +1,72 @@
-import Datalist from "../../components/datalist/datalist";
-function Cat() {
-  const header = [
-    {
-      label:'Id',
-      value : 'id'
-    },
-    {
-      label:'Nome',
-      value : 'name'
-    },
-    {
-      label:'Email',
-      value : 'email'
-    },
-    {
-      label:'Options',
-      value : ''
-    },
-  ]
 
- const data = [
-    {
-      id:'1',
-      name:'John',
-      email:'john@example.com'
-    },
-    {
-      id:'2',
-      name:'Mary',
-      email:'mary@example.com'
-    },
-    {
-      id:'3',
-      name:'Ricky',
-      email:'ricky@example.com'
-    },
-    {
-      id:'4',
-      name:'Jordan',
-      email:'jordan@example.com'
-    },
-    {
-      id:'1',
-      name:'John',
-      email:'john@example.com'
-    },
-    {
-      id:'2',
-      name:'Mary',
-      email:'mary@example.com'
-    },
-    {
-      id:'3',
-      name:'Ricky',
-      email:'ricky@example.com'
-    },
-    {
-      id:'4',
-      name:'Jordan',
-      email:'jordan@example.com'
-    },
-    
-  ]
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+
+
+function Cat() {
+
+
+  const [imagem,setImage] = useState('');
+  const [catImage,setCatImage] = useState('');
+  const [loaded,isLoaded] = useState(true);
+
+  const getGato = (async ()=>{
+    setCatImage(imagem)
+  })
+
+
 
     return (
-     <div className="">
-        <h1 className="text-3xl font-bold underline">
-          Hy yo soy Cat!
-        </h1>
+      <div className="">
+      <div className="my-6">
+          <h3 className="font-bold"> Bem Vindo a pagina de Cats</h3>
+          <span className="text-sm text-gray-500">
+              Informe algum paramentro no campo abaixo e clique em buscar.
+          </span>
+          <div className="flex justify-between my-8">
+          <div className="mx-2">
+                            <label class="block relative">
+                                <span
+                                    class="text-gray-700 bg-white px-3 -top-2 left-4 absolute text-sm"
+                                    > Status Code</span
+                                >
+                                <input
+                                  class="border rounded-lg py-4 focus:outline-white form-input mt-1 pl-6 block w-full"
+                                  type="default"
+                                  placeholder="Status Code"
+                                  value={imagem} 
+                                  name ="Status Code"
+                                  onChange={(e) => setImage(e.target.value)}
+                                />
+                            </label>
+                        </div>
+          <button type="button" className="btn-outline-primary 
+                               transition duration-300 ease-in-out 
+                               bg-blue-300 text-white
+                               focus:outline-none focus:shadow-outline border
+                               font-bold py-2 w-5/4 px-4 text-sm rounded-md flex items-center " onClick={getGato}
+                              >
+                                 Buscar
+          </button>  
+          </div>
+      </div>
+      <hr classname="my-6"/>
+      <div className="card p-8">
+         {loaded && catImage !== '' && 
+           <div className='flex justify-center'>
+             <img src={`https://http.cat/${catImage}`} 
+             className='object-cover h-3/6 w-3/6'/>
+           </div>
+           }
+         {!loaded && 
+           <div className='flex justify-center'> 
+             <p className="text-sm text-gray-500">loading...</p>
+           </div>
+         }
 
-        <div>
-          <Datalist header={header} data={data}/>
-        </div>
 
-     </div>
+      </div>
+    </div>
     );
   }
 
--- frontend/src/pages/status/status.js
+++ frontend/src/pages/status/status.js
@@ -1,9 +1,62 @@
+
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+
 function Status() {
+
+  const [imagem,setImage] = useState('');
+  const [loaded,isLoaded] = useState(true);
+  
+
+  const refresh =  ( async () =>{
+    
+    isLoaded(false)
+    
+    console.log('Fetching image')
+    const response = await axios.get('https://random.dog/woof')
+
+    console.log(response.data)
+    setImage(response.data)
+
+    isLoaded(true)
+
+
+
+  })
+
+
+
+
     return (
-     <div className="bg-red-500">
-        <h1 className="text-3xl font-bold underline">
-          Hy yo soy Status!
-        </h1>
+     <div className="">
+       <div className="my-6">
+           <h3 className="font-bold"> Bem Vindo a pagina de Dogs</h3>
+           <span className="text-sm text-gray-500">
+               Clique no botão abaixo para que uma imagem linda apareça.
+           </span>
+           <div className="flex justify-center my-3">
+           <button type="button" className="btn-outline-primary 
+                                transition duration-300 ease-in-out 
+                                bg-blue-300 text-white
+                                focus:outline-none focus:shadow-outline border
+                                font-bold py-2 w-5/4 px-4 text-sm rounded-md flex items-center " onClick={refresh}> Refresh</button>  
+           </div>
+       </div>
+       <hr classname="my-6"/>
+       <div className="card p-8">
+          {loaded && imagem !== '' && 
+            <div className='flex justify-center'>
+              <img src={'https://random.dog/'+imagem} className='flex-auto rounded-lg shadow-lg w-5/12'/>
+            </div>
+            }
+          {!loaded && 
+            <div className='flex justify-center'> 
+              <p className="text-sm text-gray-500">loading...</p>
+            </div>
+          }
+
+
+       </div>
      </div>
     );
   }
